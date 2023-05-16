0187cf15467d304417fb2e542687a678a7811c7c
MafiaPuffle <133806349+MafiaPuffle@users.noreply.github.com>
2023-05-16 16:44:29 -0700

    Newest Screens with buttons working

--- app/src/main/java/com/example/pufflemafia/HowManyCharacters.java
+++ app/src/main/java/com/example/pufflemafia/HowManyCharacters.java
@@ -2,7 +2,10 @@ package com.example.pufflemafia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HowManyCharacters extends AppCompatActivity {
 
@@ -11,6 +14,14 @@ public class HowManyCharacters extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_many_characters);
 
+        Button ReturnToChooseYourCharacterScreenWithNewValues = findViewById(R.id.NextBackToChooseYourCharacters);
+        ReturnToChooseYourCharacterScreenWithNewValues.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HowManyCharacters.this, CharacterSelectScreen.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
