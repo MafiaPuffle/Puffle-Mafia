d6de79cd8f22fb6e9e29056ce0ff0f49cc64567f
MafiaPuffle <133806349+MafiaPuffle@users.noreply.github.com>
2023-05-17 01:32:58 -0700

    Puffles are awesome

--- app/src/main/java/com/example/pufflemafia/MainMafiaPage.java
+++ app/src/main/java/com/example/pufflemafia/MainMafiaPage.java
@@ -2,7 +2,10 @@ package com.example.pufflemafia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMafiaPage extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainMafiaPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_mafia_page);
+
+        Button StartNight = findViewById(R.id.StartTheNight);
+        StartNight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMafiaPage.this, NightActions.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/pufflemafia/NightActions.java
@@ -0,0 +1,14 @@
+package com.example.pufflemafia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NightActions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_night_actions);
+    }
+}
