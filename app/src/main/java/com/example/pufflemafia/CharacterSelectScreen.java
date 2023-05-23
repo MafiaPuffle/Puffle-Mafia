891375f7d1afe93a608fd71eed4d512ca737937d
MafiaPuffle <133806349+MafiaPuffle@users.noreply.github.com>
2023-05-23 07:00:31 -0700

    Im tired

--- app/src/main/java/com/example/pufflemafia/CharacterSelectScreen.java
+++ app/src/main/java/com/example/pufflemafia/CharacterSelectScreen.java
@@ -11,6 +11,67 @@ import android.widget.ImageButton;
 import android.util.TypedValue;
 import android.widget.TextView;
 
+//public class CharacterSelectScreen extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_character_select_screen);
+//
+//        ImageButton ChosenCharacter = findViewById(R.id.NonActiveCharacter);
+//
+//        ChosenCharacter.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(CharacterSelectScreen.this, HowManyCharacters.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        GridLayout ChosenCharacterBox = findViewById(R.id.ChosenCharacterBox);
+//        ImageButton bakerButton = findViewById(R.id.Baker);
+//        countTextView = findViewById(R.id.ChosenCharacterCountText);
+//        bakerButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                addImageToGrid(ChosenCharacterBox);
+//            }
+//        });
+//
+//        //Configure Buttons
+//        configureBackToStart();
+//        configureDoneChoosingCharactersButton();
+//    }
+//
+//    private int buttonCount = 0;
+//    private TextView countTextView;
+//
+//    private void addImageToGrid(GridLayout chosencharacterBox) {
+//        ImageButton imageButton = new ImageButton(this);
+//        imageButton.setBackgroundResource(R.drawable.baker_puffle); // Set the image as the background
+//        imageButton.setImageResource(0); // Remove the image source
+//
+//        GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+//        params.rowSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
+//        params.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
+//        params.width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, getResources().getDisplayMetrics());
+//        params.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, getResources().getDisplayMetrics());
+//        imageButton.setLayoutParams(params);
+//
+//        imageButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                chosencharacterBox.removeView(v); // Remove the clicked button from the layout
+//                buttonCount--; // Decrement the counter
+//                countTextView.setText(String.valueOf(buttonCount)); // Update the count in the TextView
+//            }
+//        });
+//
+//        chosencharacterBox.addView(imageButton);
+//        buttonCount++; // Increment the counter
+//        countTextView.setText(String.valueOf(buttonCount)); // Update the count in the TextView
+//    }
+
 public class CharacterSelectScreen extends AppCompatActivity {
 
     @Override
@@ -34,21 +95,30 @@ public class CharacterSelectScreen extends AppCompatActivity {
         bakerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addImageToGrid(ChosenCharacterBox);
+                addImageToGrid(ChosenCharacterBox, R.drawable.baker_puffle);
+            }
+        });
+
+        ImageButton cupidButton = findViewById(R.id.Cupid);
+        cupidButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addImageToGrid(ChosenCharacterBox, R.drawable.cupid_puffle);
             }
         });
 
         //Configure Buttons
         configureBackToStart();
         configureDoneChoosingCharactersButton();
+        configureNonActiveCharacter();
     }
 
     private int buttonCount = 0;
     private TextView countTextView;
 
-    private void addImageToGrid(GridLayout chosencharacterBox) {
+    private void addImageToGrid(GridLayout chosenCharacterBox, int drawableId) {
         ImageButton imageButton = new ImageButton(this);
-        imageButton.setBackgroundResource(R.drawable.baker_puffle); // Set the image as the background
+        imageButton.setBackgroundResource(drawableId); // Set the image as the background
         imageButton.setImageResource(0); // Remove the image source
 
         GridLayout.LayoutParams params = new GridLayout.LayoutParams();
@@ -61,18 +131,26 @@ public class CharacterSelectScreen extends AppCompatActivity {
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                chosencharacterBox.removeView(v); // Remove the clicked button from the layout
+                chosenCharacterBox.removeView(v); // Remove the clicked button from the layout
                 buttonCount--; // Decrement the counter
                 countTextView.setText(String.valueOf(buttonCount)); // Update the count in the TextView
             }
         });
 
-        chosencharacterBox.addView(imageButton);
+        chosenCharacterBox.addView(imageButton);
         buttonCount++; // Increment the counter
         countTextView.setText(String.valueOf(buttonCount)); // Update the count in the TextView
     }
 
-
+    private void configureNonActiveCharacter() {
+        ImageButton NonActiveCharacterButton = (ImageButton) findViewById(R.id.NonActiveCharacter);
+        NonActiveCharacterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CharacterSelectScreen.this, HowManyCharacters.class));
+            }
+        });
+    }
 
 
     private void configureDoneChoosingCharactersButton() {
