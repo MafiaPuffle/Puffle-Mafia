a256a83b1d1c5432aeac35756e3e5d447a59ddf5
Digx7 <37136301+Digx7@users.noreply.github.com>
2023-05-25 22:21:37 -0700

    Got the role details screen working

--- app/src/main/java/com/example/pufflemafia/RoleDetails.java
+++ app/src/main/java/com/example/pufflemafia/RoleDetails.java
@@ -2,16 +2,45 @@ package com.example.pufflemafia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RoleDetails extends AppCompatActivity {
+
+    private Intent intent;
+
+    private ImageView selectedRoleImageView;
+    private TextView selectedRoleNameTextView;
+    private TextView selectedRoleDescriptionTextView;
+    private String roleName;
+    private Integer roleImageResourceId;
+    private String roleDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_role_details);
+
+        intent = getIntent();
+
+        selectedRoleImageView = findViewById(R.id.imageView);
+        selectedRoleNameTextView = findViewById(R.id.textView2);
+        selectedRoleDescriptionTextView = findViewById(R.id.textView3);
+
+        roleName = intent.getStringExtra("name");
+        roleImageResourceId = intent.getIntExtra("imageResourceId",0);
+        roleDescription = intent.getStringExtra("description");
+
+        selectedRoleNameTextView.setText(roleName);
+        selectedRoleImageView.setBackgroundResource(roleImageResourceId);
+        selectedRoleImageView.setImageResource(0);
+        selectedRoleDescriptionTextView.setText(roleDescription);
+
+        configureBackToMainMenu();
     }
 
     private void configureBackToMainMenu(){
--- app/src/main/java/com/example/pufflemafia/RolesScreen.java
+++ app/src/main/java/com/example/pufflemafia/RolesScreen.java
@@ -4,19 +4,66 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageButton;
+
+import com.example.pufflemafia.app.data.DataManager;
+import com.example.pufflemafia.app.data.Role;
+
+import java.util.Vector;
 
 
 public class RolesScreen extends AppCompatActivity {
+
+    private Vector<Role> allRoles;
+    private GridLayout gridLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roles);
 
+        gridLayout = findViewById(R.id.AddingTokenBox);
 
-    configureBackToMainMenu();
+        allRoles = DataManager.GetAllRoles();
+
+        for(Role role: allRoles){
+            ImageButton imageButton = addImageButtonToGrid(gridLayout, role.getImageResource());
+
+            imageButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(RolesScreen.this, RoleDetails.class);
+                    intent.putExtra("name", role.getName());
+                    intent.putExtra("imageResourceId", role.getImageResource());
+                    intent.putExtra("description", "Does something amazing!");
+                    startActivity(intent);
+                }
+            });
+        }
+
+        configureBackToMainMenu();
+    }
+
+    private ImageButton addImageButtonToGrid(GridLayout gridLayout, int drawableId) {
+        Log.d("CharacterSelectScreen", "Adding image button to grid");
+        ImageButton imageButton = new ImageButton(this);
+        imageButton.setBackgroundResource(drawableId); // Set the image as the background
+        imageButton.setImageResource(0); // Remove the image source
+
+        GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+        params.rowSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
+        params.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
+        params.width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, getResources().getDisplayMetrics());
+        params.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, getResources().getDisplayMetrics());
+        imageButton.setLayoutParams(params);
+        gridLayout.addView(imageButton);
+
+        return imageButton;
     }
 
     private void configureBackToMainMenu(){
