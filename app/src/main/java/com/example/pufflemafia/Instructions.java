77ddf23e0dd66ff54394db25c7e0aa754b7d946e
MafiaPuffle <133806349+MafiaPuffle@users.noreply.github.com>
2023-05-29 02:36:29 -0700

    added instructions kinda messed up rn

--- app/src/main/java/com/example/pufflemafia/Instructions.java
+++ app/src/main/java/com/example/pufflemafia/Instructions.java
@@ -1,23 +1,27 @@
 package com.example.pufflemafia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.pufflemafia.app.CustomAppCompatActivityWrapper;
 import com.example.pufflemafia.app.game.SoundManager;
 
-public class Instructions extends CustomAppCompatActivityWrapper {
+public class Instructions extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private ImagePagerAdapter imagePagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
 
-        configureBackButton();
+        viewPager = findViewById(R.id.viewPager);
+        imagePagerAdapter = new ImagePagerAdapter(this);
+        viewPager.setAdapter(imagePagerAdapter);
     }
 
     private void configureBackButton() {
