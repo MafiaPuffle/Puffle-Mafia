823f49a2ba116aed57f77b5eaf9a43d486b7da09
Digx7 <37136301+Digx7@users.noreply.github.com>
2023-05-30 00:39:18 -0700

    Updated text input feilds
    Tapping anywhere else on the screen now removes the keyboard when entering text into a text field

--- app/src/main/java/com/example/pufflemafia/app/CustomAppCompatActivityWrapper.java
+++ app/src/main/java/com/example/pufflemafia/app/CustomAppCompatActivityWrapper.java
@@ -1,6 +1,12 @@
 package com.example.pufflemafia.app;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,5 +21,38 @@ public class CustomAppCompatActivityWrapper extends AppCompatActivity {
         screenLifeCycleWatcher = new ScreenLifeCycleWatcher();
         getLifecycle().addObserver(screenLifeCycleWatcher);
         screenLifeCycleWatcher.Setup();
+    }
+
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) activity.getSystemService(
+                        Activity.INPUT_METHOD_SERVICE);
+        if(inputMethodManager.isAcceptingText()){
+            inputMethodManager.hideSoftInputFromWindow(
+                    activity.getCurrentFocus().getWindowToken(),
+                    0
+            );
+        }
+    }
+
+    public void makeKeyboardHidealbe(View rootView) {
+
+        // Set up touch listener for non-text box views to hide keyboard.
+        if (!(rootView instanceof EditText)) {
+            rootView.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideSoftKeyboard(CustomAppCompatActivityWrapper.this);
+                    return false;
+                }
+            });
+        }
+
+        //If a layout container, iterate over children and seed recursion.
+        if (rootView instanceof ViewGroup) {
+            for (int i = 0; i < ((ViewGroup) rootView).getChildCount(); i++) {
+                View innerView = ((ViewGroup) rootView).getChildAt(i);
+                makeKeyboardHidealbe(innerView);
+            }
+        }
     }
 }
