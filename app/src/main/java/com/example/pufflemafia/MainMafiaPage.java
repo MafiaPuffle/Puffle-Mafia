1a6301a0bf008d58cc9d714670cb4cbfbefd7c70
Digx7 <37136301+Digx7@users.noreply.github.com>
2023-05-29 23:23:59 -0700

    Players now cannot have more than one of any given token on them

--- app/src/main/java/com/example/pufflemafia/MainMafiaPage.java
+++ app/src/main/java/com/example/pufflemafia/MainMafiaPage.java
@@ -1,16 +1,14 @@
 package com.example.pufflemafia;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.pufflemafia.adaptors.PlayerDayUIAdaptor;
+import com.example.pufflemafia.adaptors.playerAdaptors.PlayerDayUIAdaptor;
 import com.example.pufflemafia.app.CustomAppCompatActivityWrapper;
 import com.example.pufflemafia.app.IListener;
 import com.example.pufflemafia.app.game.GameManager;
